a636c7ddfaaf4a1669b518b2fc63f53466246b4d
alvesmog <alvesmog@gmail.com>
2020-02-01 14:06:51 -0300

    imprimindo arrays

---
+++ aula6/array.php
@@ -0,0 +1,14 @@
+<?php
+$frutas = array('maçã','laranja','pera','morango'); $pi = 3.14;
+$professor = "Guibson Santana";
+$valorLogico = true;
+
+foreach($frutas as $fruta){
+    echo "$fruta </br>";
+}
+
+echo "$professor </br>";
+
+echo "$valorLogico";
+
+?>
--- aula6/teste.php
+++ aula6/teste.php
@@ -1,6 +1,21 @@
 <?php
 
+echo "Teste imprimindo variável no HTML: ";
+
 $x=5;
 echo $x;
+
+$n1=4;
+$n2=6;
+
+echo "</br></br>";
+
+echo "Números: ", $n1," e ",$n2;
+
+echo "<br>Soma:",$n1+$n2,"</br>";
+echo "Subtração:",$n1-$n2,"</br>";
+echo "Multiplicação:",$n1*$n2,"</br>";
+echo "Divisão:",$n1/$n2;
+
 
 ?>
